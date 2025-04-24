131d57d250dbcd415832ee2085704c22d936cb48
YokeshGaneshBabu <157607510+YokeshGaneshBabu@users.noreply.github.com>
2025-04-24 21:24:19 +0530

    Update ForwardingUnit.v

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -8,7 +8,7 @@ module ForwardingUnit (EX_MemRegwrite,EX_MemWriteReg,Mem_WbRegwrite,Mem_WbWriteR
   always@(EX_MemRegwrite or EX_MemWriteReg or Mem_WbRegwrite or Mem_WbWriteReg or ID_Ex_Rs or ID_Ex_Rt)
     
     begin
-      if(EX_MemRegwrite && EX_MemWriteReg)  //forwarding from ALU to ALU & from ALU to ID stage
+      if(EX_MemRegwrite && EX_MemWriteReg) 
         begin
           if (EX_MemWriteReg==ID_Ex_Rs)
             begin
@@ -16,7 +16,7 @@ module ForwardingUnit (EX_MemRegwrite,EX_MemWriteReg,Mem_WbRegwrite,Mem_WbWriteR
             upperMux_sel<=2'b10;
             comparatorMux1Selector<=2'b01;
             end
-          else //no forwarding
+          else 
           begin
           upperMux_sel<=2'b00;
           comparatorMux1Selector<=2'b00;
@@ -27,7 +27,7 @@ module ForwardingUnit (EX_MemRegwrite,EX_MemWriteReg,Mem_WbRegwrite,Mem_WbWriteR
             lowerMux_sel<=2'b10;
             comparatorMux2Selector<=2'b01;
               end
-          else //no forwarding
+          else
           begin
           lowerMux_sel<=2'b00;
           comparatorMux2Selector<=2'b00;
@@ -35,14 +35,14 @@ module ForwardingUnit (EX_MemRegwrite,EX_MemWriteReg,Mem_WbRegwrite,Mem_WbWriteR
             
         end
       
-      else if (Mem_WbRegwrite && Mem_WbWriteReg)   //forwarding from Memorystage to ALU & from Memorystage to ID stage
+      else if (Mem_WbRegwrite && Mem_WbWriteReg)   
         begin
           if ((Mem_WbWriteReg==ID_Ex_Rs) && (EX_MemWriteReg!=ID_Ex_Rs))
             begin
             upperMux_sel<=2'b01;
             comparatorMux1Selector<=2'b10;
             end
-          else //no forwarding
+          else 
           begin
           upperMux_sel<=2'b00;
           comparatorMux1Selector<=2'b00;
@@ -56,7 +56,7 @@ module ForwardingUnit (EX_MemRegwrite,EX_MemWriteReg,Mem_WbRegwrite,Mem_WbWriteR
             comparatorMux2Selector<=2'b10;
           end
 
-          else //no forwarding
+          else 
           begin
           lowerMux_sel<=2'b00;
           comparatorMux2Selector<=2'b00;
@@ -65,7 +65,7 @@ module ForwardingUnit (EX_MemRegwrite,EX_MemWriteReg,Mem_WbRegwrite,Mem_WbWriteR
           
         end
       
-      else    //No forwarding
+      else   
         begin
                       upperMux_sel<=2'b00;
                       lowerMux_sel<=2'b00;
